6e45de39b6a8afb33e42c3c4d1f3700b360de0c6
Jiahua Jiang <jiahua212@gmail.com>
2024-09-11 15:37:19 -0700

    using uid for path

--- terraform/s3/backend.tf
+++ terraform/s3/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraformstatebucket120"
-    key    = "terraform/statefile.tfstate"
+    key    = "terraform/statefile${var.uid}.tfstate"
     region = "us-west-2"
   }
 }
