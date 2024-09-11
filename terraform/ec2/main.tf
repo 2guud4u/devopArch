079f0a26f0054e439d5128b0ca385344d2bf40a0
Jiahua Jiang <jiahua212@gmail.com>
2024-09-11 15:18:06 -0700

    separated different resources

---
+++ terraform/ec2/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+variable "region" { type= string }
+variable "instance_type" { type= string }
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
---
+++ terraform/s3/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+variable "bucketName" { type= string } 
+variable "region" { type= string }
+
+
+provider "aws" {
+  region  = var.region
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucketName  # Change this to your desired bucket name
+}
